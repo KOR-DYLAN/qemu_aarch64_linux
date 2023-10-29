96ee56eed64c4d04ffec4e1791cf8c07a0d1770a
JunKi, Hong <dylanhong920509@gmail.com>
2023-10-29 20:07:29 +0900

    [linux & script] add feature ' extract and apply device tree'

--- scripts/run/qemu.mk
+++ scripts/run/qemu.mk
@@ -2,14 +2,14 @@ QEMU		:=$(QDIR)/qemu-system-aarch64
 MACHIN		:=virt
 CPU			:=cortex-a72
 GIC			:=2
-SMP			:=2
+SMP			:=4
 RAM			:=1G
 VIRT		:=off
 SECURE		:=on
 BIOS		:=bl1.bin
 IMG			:=$(KDIR)/arch/arm64/boot/Image
-FDT_BASE	:=$(TOPDIR)/resource/qemu-system-aarch64
-FDT			:=$(FDT_BASE).dtb
+FDT_NAME	:=qemu-system-aarch64
+FDT			:=$(KDIR)/arch/arm64/boot/dts/qemu/$(FDT_NAME).dtb
 ROOTFS		:=$(BDIR)/images/rootfs.ext4
 BOOTARGS	:="rootwait root=/dev/vda console=ttyAMA0"
 QFLAG		:=-netdev user,id=eth0 -device virtio-net-device,netdev=eth0
@@ -19,11 +19,12 @@ QFLAG		+=-d unimp -semihosting-config enable=on
 DEBUG_IMG	:=bl2.bin
 
 run: $(IMG) $(ROOTFS)
-	$(QEMU) -M $(MACHIN),gic-version=$(GIC),virtualization=$(VIRT),secure=$(SECURE),acpi=off -cpu $(CPU) -smp $(SMP) -m $(RAM) -bios $(BIOS) -kernel $(IMG) -append $(BOOTARGS) $(QFLAG) -nographic
+	$(QEMU) -M $(MACHIN),gic-version=$(GIC),virtualization=$(VIRT),secure=$(SECURE),acpi=off -cpu $(CPU) -smp $(SMP) -m $(RAM) -bios $(BIOS) -dtb $(FDT) -kernel $(IMG) -append $(BOOTARGS) $(QFLAG) -nographic
 
 debug: $(DEBUG_IMG) $(ROOTFS)
-	$(QEMU) -M $(MACHIN),gic-version=$(GIC),virtualization=$(VIRT),secure=$(SECURE),acpi=off -cpu $(CPU) -smp $(SMP) -m $(RAM) -bios $(BIOS) -kernel $(DEBUG_IMG) -append $(BOOTARGS) $(QFLAG) -nographic -S -s
+	$(QEMU) -M $(MACHIN),gic-version=$(GIC),virtualization=$(VIRT),secure=$(SECURE),acpi=off -cpu $(CPU) -smp $(SMP) -m $(RAM) -bios $(BIOS) -dtb $(FDT) -kernel $(DEBUG_IMG) -append $(BOOTARGS) $(QFLAG) -nographic -S -s
 
 extract_fdt:
-	$(QEMU) -M $(MACHIN) -cpu $(CPU) -smp $(SMP) -m $(RAM) $(QFLAG) -machine dumpdtb=$(FDT)
-	dtc -I dtb -O dts -o $(FDT_BASE).dts $(FDT_BASE).dtb
+	$(QEMU) -M $(MACHIN),gic-version=$(GIC),virtualization=$(VIRT),secure=$(SECURE),acpi=off,dumpdtb=resource/$(FDT_NAME).dtb -cpu $(CPU) -smp $(SMP) -m $(RAM) -kernel $(IMG) -append $(BOOTARGS) -nographic
+	dtc -I dtb -O dts -o resource/$(FDT_NAME).dts resource/$(FDT_NAME).dtb
+	rm -f resource/$(FDT_NAME).dtb
